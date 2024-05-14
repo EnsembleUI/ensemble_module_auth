567eece4a79d5b3049aa662822651aeba37f7ebd
snehmehta <snehmehta933@gmail.com>
2024-05-14 19:17:28 +0530

    added anonymous login with firebase

--- lib/connect/OAuthController.dart
+++ lib/connect/OAuthController.dart
@@ -22,13 +22,16 @@ class OAuthControllerImpl implements OAuthController {
   static const refreshTokenKey = '_refreshToken';
 
   @override
-  Future<OAuthServiceToken?> authorize(BuildContext context, OAuthService service,
-      {required String scope, bool forceNewTokens = false,
+  Future<OAuthServiceToken?> authorize(
+      BuildContext context, OAuthService service,
+      {required String scope,
+      bool forceNewTokens = false,
       InvokeAPIAction? tokenExchangeAPI}) async {
     // see if the tokens already exists
     const storage = FlutterSecureStorage();
     if (!forceNewTokens) {
-      String? accessToken = await storage.read(key: service.name + accessTokenKey);
+      String? accessToken =
+          await storage.read(key: service.name + accessTokenKey);
       String? refreshToken =
           await storage.read(key: service.name + refreshTokenKey);
       if (accessToken != null) {
@@ -39,9 +42,11 @@ class OAuthControllerImpl implements OAuthController {
 
     ServiceCredentialPayload? servicePayload = getServicePayload(service);
     if (servicePayload != null) {
-      OAuthCredential? credential = servicePayload.credential.platformCredential;
+      OAuthCredential? credential =
+          servicePayload.credential.platformCredential;
       if (credential == null) {
-        throw RuntimeError('OAuth Service is not correctly setup. Please check your configuration');
+        throw RuntimeError(
+            'OAuth Service is not correctly setup. Please check your configuration');
       }
 
       String codeVerifier = _generateCodeVerifier();
@@ -63,22 +68,21 @@ class OAuthControllerImpl implements OAuthController {
 
       // authorize with the service
       final result = await FlutterWebAuth2.authenticate(
-          url: uri.toString(),
-          callbackUrlScheme: credential.redirectScheme);
+          url: uri.toString(), callbackUrlScheme: credential.redirectScheme);
       final resultUri = Uri.parse(result);
       String? code = resultUri.queryParameters['code'];
       if (code != null && state == resultUri.queryParameters['state']) {
-
         // code exchange can be on the client or server
         OAuthServiceToken? token;
         if (tokenExchangeAPI == null) {
-          token = await _getTokenFromClient(code: code,
-              codeVerifier: codeVerifier, tokenURL: servicePayload.tokenURL,
+          token = await _getTokenFromClient(
+              code: code,
+              codeVerifier: codeVerifier,
+              tokenURL: servicePayload.tokenURL,
               service: service,
               oauthCredential: credential);
         } else {
-          token = await _getTokenFromServer(
-              context,
+          token = await _getTokenFromServer(context,
               code: code,
               codeVerifier: codeVerifier,
               tokenExchangeAPI: tokenExchangeAPI);
@@ -112,12 +116,13 @@ class OAuthControllerImpl implements OAuthController {
 
   /// exchange OAuth code for token on the server
   Future<OAuthServiceToken?> _getTokenFromServer(BuildContext context,
-      {required String code, required String codeVerifier,
-        required InvokeAPIAction tokenExchangeAPI}) async {
-
+      {required String code,
+      required String codeVerifier,
+      required InvokeAPIAction tokenExchangeAPI}) async {
     try {
       Response? response = await InvokeAPIController().executeWithContext(
-          context, tokenExchangeAPI, additionalInputs: { 'code': code, 'codeVerifier': codeVerifier });
+          context, tokenExchangeAPI,
+          additionalInputs: {'code': code, 'codeVerifier': codeVerifier});
       // even when server received the access/refresh tokens, it has the option
       // to NOT send them to us if it doesn't make sense (e.g.g client doesn't
       // make any API call). We'll still mark this call as success by returning
@@ -134,130 +139,127 @@ class OAuthControllerImpl implements OAuthController {
     return null;
   }
 
+  // var data = json.encode({
+  //   'code': code,
+  //   'serviceId': service.name,
+  //   'token': JWT({'redirectUri': servicePayload.redirectUri})
+  //       .sign(SecretKey(dotenv.env['OAUTH_TOKEN']!))
+  // });
+  // var response = await http.post(Uri.parse(tokenExchangeServer),
+  //     body: data, headers: {'Content-Type': 'application/json'});
+  // if (response.statusCode == 200) {
+  //   var jsonResponse = json.decode(response.body);
+  //   if (jsonResponse != null) {
+  //     return OAuthServiceToken(
+  //         accessToken: jsonResponse['access_token'],
+  //         refreshToken: jsonResponse['refresh_token']);
+  //   }
+  // }
+  // return null;
+}
 
-
-
-    // var data = json.encode({
-    //   'code': code,
-    //   'serviceId': service.name,
-    //   'token': JWT({'redirectUri': servicePayload.redirectUri})
-    //       .sign(SecretKey(dotenv.env['OAUTH_TOKEN']!))
-    // });
-    // var response = await http.post(Uri.parse(tokenExchangeServer),
-    //     body: data, headers: {'Content-Type': 'application/json'});
-    // if (response.statusCode == 200) {
-    //   var jsonResponse = json.decode(response.body);
-    //   if (jsonResponse != null) {
-    //     return OAuthServiceToken(
-    //         accessToken: jsonResponse['access_token'],
-    //         refreshToken: jsonResponse['refresh_token']);
-    //   }
-    // }
-    // return null;
-  }
-
-  /// exchange OAuth code for token locally
-  Future<OAuthServiceToken?> _getTokenFromClient({
-      required String code,
-      required String codeVerifier,
-      required String tokenURL,
-      required OAuthService service,
-      required OAuthCredential oauthCredential}) async {
-
-    var body = {
-      'client_id': oauthCredential.clientId,
-      'redirect_uri': oauthCredential.redirectUri,
-      'grant_type': 'authorization_code',
-      'code': code,
-      'code_verifier': codeVerifier
-    };
-    // inject the client secret on Web. Note that PKCE technically don't required
-    // clientSecret, but some clients (i.e. Google) still require it.
-    if (kIsWeb) {
-      String? webClientSecret = await StorageManager().readSecurely('SERVICES_OAUTH_${service.name}_WEB_CLIENT_SECRET');
-      if (webClientSecret != null && webClientSecret.isNotEmpty) {
-        body['client_secret'] = webClientSecret;
-      }
+/// exchange OAuth code for token locally
+Future<OAuthServiceToken?> _getTokenFromClient(
+    {required String code,
+    required String codeVerifier,
+    required String tokenURL,
+    required OAuthService service,
+    required OAuthCredential oauthCredential}) async {
+  var body = {
+    'client_id': oauthCredential.clientId,
+    'redirect_uri': oauthCredential.redirectUri,
+    'grant_type': 'authorization_code',
+    'code': code,
+    'code_verifier': codeVerifier
+  };
+  // inject the client secret on Web. Note that PKCE technically don't required
+  // clientSecret, but some clients (i.e. Google) still require it.
+  if (kIsWeb) {
+    String? webClientSecret = await StorageManager()
+        .readSecurely('SERVICES_OAUTH_${service.name}_WEB_CLIENT_SECRET');
+    if (webClientSecret != null && webClientSecret.isNotEmpty) {
+      body['client_secret'] = webClientSecret;
     }
+  }
 
-    final response = await http.post(Uri.parse(tokenURL), body: body);
-    if (response.statusCode >= 200 && response.statusCode <= 299) {
-      var jsonResponse = json.decode(response.body);
-      if (jsonResponse != null) {
-        return OAuthServiceToken(
-            accessToken: jsonResponse['access_token'],
-            refreshToken: jsonResponse['refresh_token']);
-      }
+  final response = await http.post(Uri.parse(tokenURL), body: body);
+  if (response.statusCode >= 200 && response.statusCode <= 299) {
+    var jsonResponse = json.decode(response.body);
+    if (jsonResponse != null) {
+      return OAuthServiceToken(
+          accessToken: jsonResponse['access_token'],
+          refreshToken: jsonResponse['refresh_token']);
     }
-    return null;
   }
+  return null;
+}
 
-  /// generate a unique state
-  String generateState() {
-    var raw = List<int>.generate(32, (index) => Random.secure().nextInt(256));
-    return base64Url.encode(raw);
+/// generate a unique state
+String generateState() {
+  var raw = List<int>.generate(32, (index) => Random.secure().nextInt(256));
+  return base64Url.encode(raw);
+}
+
+ServiceCredentialPayload? getServicePayload(OAuthService service) {
+  if (service == OAuthService.google) {
+    return getGoogleServicePayload();
+  } else if (service == OAuthService.microsoft) {
+    return getMicrosoftServicePayload();
+  } else if (service == OAuthService.yahoo) {
+    return getYahooServicePayload();
   }
+  return null;
+}
 
-  ServiceCredentialPayload? getServicePayload(OAuthService service) {
-    if (service == OAuthService.google) {
-      return getGoogleServicePayload();
-    } else if (service == OAuthService.microsoft) {
-      return getMicrosoftServicePayload();
-    } else if (service == OAuthService.yahoo) {
-      return getYahooServicePayload();
+/// These will come from our server
+ServiceCredentialPayload? getGoogleServicePayload() {
+  ServiceCredential? credential = _getServiceCredential(OAuthService.google);
+  if (credential != null) {
+    bool offline = credential.config?['offline'] == true;
+    return ServiceCredentialPayload(
+        credential: credential,
+        authorizationURL:
+            "https://accounts.google.com/o/oauth2/v2/auth${offline ? '?access_type=offline&prompt=consent' : ''}",
+        tokenURL: "https://oauth2.googleapis.com/token");
+  }
+  return null;
+}
+
+ServiceCredentialPayload? getMicrosoftServicePayload() {
+  ServiceCredential? credential = _getServiceCredential(OAuthService.microsoft);
+  if (credential != null) {
+    String? tenantId = credential.config?['tenantId'];
+    if (tenantId == null) {
+      throw RuntimeError('tenantId is required for connecting to Microsoft.');
     }
-    return null;
+    return ServiceCredentialPayload(
+        credential: credential,
+        authorizationURL:
+            'https://login.microsoftonline.com/$tenantId/oauth2/v2.0/authorize?prompt=select_account',
+        tokenURL:
+            'https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token');
   }
+  return null;
+}
 
-  /// These will come from our server
-  ServiceCredentialPayload? getGoogleServicePayload() {
-    ServiceCredential? credential = _getServiceCredential(OAuthService.google);
-    if (credential != null) {
-      bool offline = credential.config?['offline'] == true;
-      return ServiceCredentialPayload(
-          credential: credential,
-          authorizationURL:
-              "https://accounts.google.com/o/oauth2/v2/auth${offline ? '?access_type=offline&prompt=consent' : ''}",
-          tokenURL: "https://oauth2.googleapis.com/token");
-    }
-    return null;
+ServiceCredentialPayload? getYahooServicePayload() {
+  ServiceCredential? credential = _getServiceCredential(OAuthService.yahoo);
+  if (credential != null) {
+    return ServiceCredentialPayload(
+        credential: credential,
+        authorizationURL: 'https://api.login.yahoo.com/oauth2/request_auth',
+        tokenURL: '// to be added');
   }
+  return null;
+}
 
-  ServiceCredentialPayload? getMicrosoftServicePayload() {
-    ServiceCredential? credential = _getServiceCredential(OAuthService.microsoft);
-    if (credential != null) {
-      String? tenantId = credential.config?['tenantId'];
-      if (tenantId == null) {
-        throw RuntimeError('tenantId is required for connecting to Microsoft.');
-      }
-      return ServiceCredentialPayload(
-          credential: credential,
-          authorizationURL: 'https://login.microsoftonline.com/$tenantId/oauth2/v2.0/authorize?prompt=select_account',
-          tokenURL: 'https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token');
-    }
-    return null;
-  }
-
-  ServiceCredentialPayload? getYahooServicePayload() {
-    ServiceCredential? credential = _getServiceCredential(OAuthService.yahoo);
-    if (credential != null) {
-      return ServiceCredentialPayload(
-          credential: credential,
-          authorizationURL: 'https://api.login.yahoo.com/oauth2/request_auth',
-          tokenURL: '// to be added'
-      );
-    }
-    return null;
-  }
-
-  ServiceCredential? _getServiceCredential(OAuthService service) =>
-      Ensemble().getServices()?.getServiceCredential(service);
-
+ServiceCredential? _getServiceCredential(OAuthService service) =>
+    Ensemble().getServices()?.getServiceCredential(service);
 
 /// add authorization URL and token URL
 class ServiceCredentialPayload {
-  ServiceCredentialPayload({
-      required this.credential,
+  ServiceCredentialPayload(
+      {required this.credential,
       required this.authorizationURL,
       required this.tokenURL});
 
